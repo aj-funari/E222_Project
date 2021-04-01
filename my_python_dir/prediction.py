c9a6f05da6fa5f7cf333ae47832bc2f97095d2ce
AJ Funari <ajfunari@gmail.com>
2021-03-31 22:09:14 -0400

    endpoints

--- my_python_dir/prediction.py
+++ my_python_dir/prediction.py
@@ -14,11 +14,15 @@ def my_prediction(arg):
     b = dummyT.shape
     a_str = json.dumps(a)
     b_str = json.dumps(b)
-    prediction = my_mdl.predict(dummyT)
-    return(prediction)
+    pred = my_mdl.predict(dummyT)
+    pred = pred.tolist()
+    pred = json.dumps(pred)
+    return pred
 
-def score(test_size):
-    x_train, x_test, y_train, y_test = train_test_split(X_new, Y_new, test_size, random_state=0)
+def score(arg):
+    x_train, x_test, y_train, y_test = train_test_split(X_new, Y_new, test_size=arg, random_state=0)
     my_mdl = pipe.fit(x_train, y_train)
     score = my_mdl.score(x_test,y_test)
-    return(score)
+    score = score.tolist()
+    score = json.dumps(score)
+    return score
