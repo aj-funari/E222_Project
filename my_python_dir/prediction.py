8888ebe229d204e18aa2a40bcefc0370b024d832
AJ Funari <ajfunari@gmail.com>
2021-04-01 14:45:58 -0400

    first endpoint

--- my_python_dir/prediction.py
+++ my_python_dir/prediction.py
@@ -5,7 +5,7 @@ import json
 
 #load the model
 
-my_model = load('logistic.pk1')
+my_model = load('/predict_test/my_python_dir/logistic.pkl')
 
 def my_prediction(arg):
     dummy = np.array(arg)
@@ -14,15 +14,16 @@ def my_prediction(arg):
     b = dummyT.shape
     a_str = json.dumps(a)
     b_str = json.dumps(b)
-    pred = my_mdl.predict(dummyT)
+    pred = my_model.predict(dummyT)
     pred = pred.tolist()
     pred = json.dumps(pred)
     return pred
 
 def score(arg):
     x_train, x_test, y_train, y_test = train_test_split(X_new, Y_new, test_size=arg, random_state=0)
-    my_mdl = pipe.fit(x_train, y_train)
+    my_model = pipe.fit(x_train, y_train)
     score = my_mdl.score(x_test,y_test)
     score = score.tolist()
     score = json.dumps(score)
     return score
+
